669d031892bc8dd9fb402a1f90ff43f97bd19d24
Bohdan <bodya.flesh@gmail.com>
2017-07-10 02:47:46 +0300

    up

--- test/fs/app2.js
+++ test/fs/app2.js
@@ -1,7 +1,10 @@
 const fs = require('fs');
+const zlib = require('zlib');
 
-let readable = fs.createReadStream(__dirname + '/greet.txt', {encoding : 'utf8', highWaterMark: 1024});
-
+let readable = fs.createReadStream(__dirname + '/greet.txt');
 let writable = fs.createWriteStream(__dirname + '/greetcopy.txt');
+let compressed = fs.createWriteStream(__dirname + '/greet.txt.gz');
+const gzip = zlib.createGzip();
 
 readable.pipe(writable);
+readable.pipe(gzip).pipe(compressed);
