6b79165aa0b7f7425416268b2ae433b5e90507dd
Bohdan <bodya.flesh@gmail.com>
2017-07-17 14:16:00 +0300

    up

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,11 +1,33 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
+
+const port = process.env.PORT || 3000;
 
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials/');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now} : ${req.method} ${req.url}`;
+
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err){
+            console.log('Unable to append to server.log');
+        }
+    });
+    next();
+});
+
+/*
+app.use((req, res, next) => {
+    res.render('maintence.hbs');
+});
+*/
 
 hbs.registerHelper('getCurrentYear', () => {
     return new Date().getFullYear();
@@ -34,4 +56,4 @@ app.get('/bad', () => {
     });
 });
 
-app.listen(3000);
+app.listen(port);
