01bc1b35d718939bc9725028c54cf2906a584446
Bohdan <bodya.flesh@gmail.com>
2017-07-17 03:23:10 +0300

    up

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,19 +1,31 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials/');
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
 app.get('/', (req, res) => {
-    //res.send('Hello Express!');
-    res.send({
-        name: 'Bohdan',
-        likes: ['Nuts', 'Apples']
-    })
+    res.render('home.hbs', {
+        pageTitle: 'Home page',
+        welcomeMessage: 'hello world!'    
+    });
 });
 
 app.get('/about', (req, res) => {
-    res.send('About Page');
+    res.render('about.hbs', {
+        pageTitle: 'About page'
+    });
 });
 
 app.get('/bad', () => {
