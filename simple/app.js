ccfd67d0d996069d43c8c370a7fdd35736df3c57
Bohdan <bodya.flesh@gmail.com>
2017-11-24 01:28:14 +0200

    fixed bags

--- simple/app.js
+++ simple/app.js
@@ -20,7 +20,7 @@ require('./config/passport')(passport);
 //map global promise -get rid of warning
 mongoose.Promise = global.Promise;
 //conect to mongoose
-mongoose.connect('mongodb://localhost/vidjot-dev', {
+mongoose.connect('mongodb://bodya.flesh:123123@ds055872.mlab.com:55872/vidjot-prod', {
   useMongoClient: true
 })
 .then(() => console.log('MongoDB Connected...'))
@@ -79,7 +79,7 @@ app.get('/about', (req, res) => {
 app.use('/ideas', ideas);
 app.use('/users', users);
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server started on post ${port}`);
