465f89578ed91debd58a310a8ce66d558ce7cea0
Bohdan <bodya.flesh@gmail.com>
2019-11-16 19:55:00 +0200

    update

--- resfull-api-node-clear/app/index.js
+++ resfull-api-node-clear/app/index.js
@@ -15,11 +15,19 @@ const server = http.createServer(function(req, res) {
     var path = parseUrl.pathname;
     var trimmedPath = path.replace(/^\/+|\/+$/g, "");
 
+    // Get the query string as an object
+    var quertStringObject = parseUrl.query;
+
+    //Get the HTTP Method
+    var method = req.method.toLowerCase();
+
     // Send the response
     res.end("Hello world\n");
 
     // Log the request path
     console.log("request received on path:", trimmedPath);
+    console.log("Method:", method);
+    console.log(quertStringObject);
 });
 
 //start the server, and have it listen on port 3000
