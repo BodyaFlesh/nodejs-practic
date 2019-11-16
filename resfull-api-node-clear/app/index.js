8fbcbd23cfc3025cbcaa57762acbb3ef3a2f857d
Bohdan <bodya.flesh@gmail.com>
2019-11-16 12:29:30 +0200

    get url request

--- resfull-api-node-clear/app/index.js
+++ resfull-api-node-clear/app/index.js
@@ -4,10 +4,22 @@
 
 //Dependencies
 const http = require("http");
+const url = require("url");
 
 //the server should respond to all requests with a string
 const server = http.createServer(function(req, res) {
+    // Get the URL and parse it
+    var parseUrl = url.parse(req.url, true);
+
+    // Get the path
+    var path = parseUrl.pathname;
+    var trimmedPath = path.replace(/^\/+|\/+$/g, "");
+
+    // Send the response
     res.end("Hello world\n");
+
+    // Log the request path
+    console.log("request received on path:", trimmedPath);
 });
 
 //start the server, and have it listen on port 3000
