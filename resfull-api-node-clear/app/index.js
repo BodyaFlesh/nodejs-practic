ca1f9eab46e7fae39af9a1e3a13214115ce3d742
Bohdan <bodya.flesh@gmail.com>
2019-11-28 01:12:01 +0200

    update

--- resfull-api-node-clear/app/index.js
+++ resfull-api-node-clear/app/index.js
@@ -116,17 +116,22 @@ var unifuedServer = function(req, res) {
 var handlers = {};
 
 //sample heandler
-handlers.sample = function(data, callback) {
-    //callback a http status code, and a payload object
-    callback(406, { name: "sample handler" });
-};
+// handlers.sample = function(data, callback) {
+//     //callback a http status code, and a payload object
+//     callback(406, { name: "sample handler" });
+// };
 
 //not found handler
 handlers.notFound = function(data, callback) {
     callback(404);
 };
 
+//ping handler
+handlers.ping = function(data, callback) {
+    callback(200);
+};
+
 //define a request router
 var router = {
-    sample: handlers.sample
+    ping: handlers.ping
 };
