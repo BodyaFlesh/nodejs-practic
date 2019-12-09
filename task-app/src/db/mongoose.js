5037dfacea62f473d56aee899491839b6f80b0ae
Bohdan <bodya.flesh@gmail.com>
2019-12-09 23:07:50 +0200

    update

--- task-app/src/db/mongoose.js
+++ task-app/src/db/mongoose.js
@@ -1,26 +1,50 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
     useNewUrlParser: true,
     useCreateIndex: true
-})
+});
 
-const User = mongoose.model('User', {
+const User = mongoose.model("User", {
     name: {
         type: String
     },
     age: {
         type: Number
     }
-})
+});
+
+const Taks = mongoose.model("task", {
+    description: {
+        type: String
+    },
+    completed: {
+        type: Boolean
+    }
+});
+
+const tast = new Taks({
+    description: "test",
+    completed: true
+});
+
+tast.save()
+    .then(() => {
+        console.log(me);
+    })
+    .catch(error => {
+        console.log("Error!", error);
+    });
 
 const me = new User({
-    name: 'Andrew',
-    age: 'Mike'
-})
+    name: "Andrew",
+    age: 18
+});
 
-me.save().then(() => {
-    console.log(me)
-}).catch((error) => {
-    console.log('Error!', error)
-})
+me.save()
+    .then(() => {
+        console.log(me);
+    })
+    .catch(error => {
+        console.log("Error!", error);
+    });
