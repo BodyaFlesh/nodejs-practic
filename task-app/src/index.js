81868af219d6693a1bde13da2a8b5ac981c94a8b
Bohdan <bodya.flesh@gmail.com>
2019-12-11 00:58:17 +0200

    and point task read

--- task-app/src/index.js
+++ task-app/src/index.js
@@ -9,7 +9,6 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 app.post("/users", (req, res) => {
-    const user = new User(req.body);
     console.log(req.body);
 
     user.save()
@@ -18,6 +17,32 @@ app.post("/users", (req, res) => {
         })
         .catch(e => {
             res.status(400).send(e);
+        });
+});
+
+app.get("/users", (req, res) => {
+    User.find({})
+        .then(users => {
+            res.send(users);
+        })
+        .catch(e => {
+            res.status(500);
+        });
+});
+
+app.get("/users/:id", (req, res) => {
+    const _id = req.params.id;
+
+    User.findById(_id)
+        .then(user => {
+            if (!user) {
+                return res.status(404).send();
+            }
+
+            res.send(user);
+        })
+        .catch(e => {
+            res.status(500).send();
         });
 });
 
@@ -30,6 +55,32 @@ app.post("/tasks", (req, res) => {
         })
         .catch(e => {
             res.status(400).send(e);
+        });
+});
+
+app.get("/tasks", (req, res) => {
+    Task.find({})
+        .then(tasks => {
+            res.send(tasks);
+        })
+        .catch(e => {
+            res.status(500);
+        });
+});
+
+app.get("/tasks/:id", (req, res) => {
+    const _id = req.params.id;
+
+    Task.findById(_id)
+        .then(task => {
+            if (!task) {
+                return res.status(404);
+            }
+
+            res.send(task);
+        })
+        .catch(e => {
+            res.status(500);
         });
 });
 
