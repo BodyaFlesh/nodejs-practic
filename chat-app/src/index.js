446f324e385fefe441650a465786538737191a97
Bohdan <bodya.flesh@gmail.com>
2019-12-20 09:40:40 +0200

    update

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -17,12 +17,12 @@ let count = 0;
 io.on("connection", socket => {
     console.log("New WebSocket connection");
 
-    socket.emit("countUpdated", count);
+    // socket.emit("countUpdated", count);
 
-    socket.on("increment", () => {
-        count++;
-        io.emit("countUpdated", count);
-    });
+    // socket.on("increment", () => {
+    //     count++;
+    //     io.emit("countUpdated", count);
+    // });
 });
 
 server.listen(port, () => {
