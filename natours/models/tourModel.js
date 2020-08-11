a061741be8f22bd51da0cda442510d1198617554
Bohdan Sych <bodya.flesh@gmail.com>
2020-08-11 17:35:33 +0300

    update

--- natours/models/tourModel.js
+++ natours/models/tourModel.js
@@ -90,7 +90,12 @@ tourSchema.pre(/^find/, function (next) {
     next();
 });
 
-// tourSchema.post(/^find/)
+// Aggregation middleware
+tourSchema.pre('affregate', function (next) {
+    this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+    console.log(this);
+    next();
+});
 
 const Tour = mongoose.model('Tour', tourSchema);
 
