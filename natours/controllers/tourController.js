ed73bc3b390463219a04d34c3ee979eb739595db
Bohdan Sych <bodya.flesh@gmail.com>
2020-08-11 05:42:02 +0300

    update

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -11,8 +11,27 @@ exports.getAllTours = async (req, res) => {
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach(el => delete queryObj[el]);
 
+    let queryStr = JSON.stringiy(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+
     try {
-        const tours = await Tour.find(queryObj);
+        const query = Tour.find(JSON.parse(queryStr));
+
+        if(req.query.sort){
+            const sortBy = req.query.sort.split(',').join(' ');
+            query = query.sort(sortBy);
+        }else{
+            query = query.sort('-createdAt');
+        }
+
+        if(req.query.fields){
+            const fields = req.query.fields.split(',').join(' ');
+            query = query.select(fields);
+        }else{
+            query = query.select('-__v');
+        }
+
+        const tours = await query;
 
         res.status(200).json({
             status: 'success',
