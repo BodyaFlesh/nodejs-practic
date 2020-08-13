bf616906730dfdacaaa9394fb2dd40347fbb8516
Bohdan Sych <bodya.flesh@gmail.com>
2020-08-13 21:19:02 +0300

    unhandledRejection

--- natours/server.js
+++ natours/server.js
@@ -18,6 +18,15 @@ mongoose.connect(process.env.DATABASE, {
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
     //console.log(`App running on port ${port}`);
+});
+
+
+process.on('unhandledRejection', err => {
+    console.log(err.name, err.message);
+    console.log('Unhandler rejection! Shutting down...');
+    server.close(() => {
+        process.exit(1);
+    });
 });
