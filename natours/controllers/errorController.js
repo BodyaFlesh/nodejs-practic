f653c99cc6797be7c0e5f6e0acf7b39d3c88aa8a
Bohdan Sych <bodya.flesh@gmail.com>
2020-08-13 09:58:20 +0300

    update Error handler

--- natours/controllers/errorController.js
+++ natours/controllers/errorController.js
@@ -1,9 +1,40 @@
+const sendErrorDev = (err, res) => {
+    res.status(err.statusCode).json({
+        status: err.status,
+        messag: err.message,
+        error: err,
+        stack: err.stack
+    });
+}
+
+const sendErrorProd = (err, res) => {
+    // Operational, trusted error: send message to client
+    if (err.isOperational) {
+        res.status(err.statusCode).json({
+            status: err.status,
+            messag: err.message
+        });
+    }
+    // Programming or other unknown errorL don't leak error defails
+    else {
+        console.error('Error', err);
+
+        res.status(500).json({
+            status: 'error',
+            message: 'Something went wrong!'
+        })
+    }
+
+
+};
+
 module.exports = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'error';
 
-    res.status(err.statusCode).json({
-        status: err.status,
-        messag: err.message
-    });
+    if (process.env.NODE_ENV == 'development') {
+        sendErrorDev(err, res);
+    } else if (process.env.NODE_ENV == 'production') {
+        sendErrorProd(err, ress);
+    }
 }
