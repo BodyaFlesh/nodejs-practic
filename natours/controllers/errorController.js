5e1efcac692a68a230d2a467b8af7a45b84fa92b
Bohdan Sych <bodya.flesh@gmail.com>
2020-08-13 21:10:57 +0300

    update error handler

--- natours/controllers/errorController.js
+++ natours/controllers/errorController.js
@@ -12,6 +12,14 @@ const handleDuplicateFieldsDB = err => {
     return new AppError(message, 400);
 }
 
+const handleValidationErrorDB = err => {
+    const errors = Object.values(err.errors).map(val => el.message);
+
+    const message = `Invalid input data. ${errors.join('. ')}`;
+
+    return new AppError(message, 400);
+}
+
 const sendErrorDev = (err, res) => {
     res.status(err.statusCode).json({
         status: err.status,
@@ -52,6 +60,10 @@ module.exports = (err, req, res, next) => {
         let error = { ...err };
         if (error.name === "CastError") error = handleCastErrorDB(error);
         if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+        if(error.name === 'ValidationError'){
+            error = handleValidationErrorDB(error);
+        }
+
         sendErrorProd(error, ress);
     }
 }
--- natours/utils/apiFeatures.js
+++ natours/utils/apiFeatures.js
@@ -10,7 +10,7 @@ class APIFeatures {
         const excludedFields = ['page', 'sort', 'limit', 'fields'];
         excludedFields.forEach(el => delete queryObj[el]);
 
-        let queryStr = JSON.stringiy(queryObj);
+        let queryStr = JSON.stringify(queryObj);
         queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
 
         this.query.find(JSON.parse(queryStr));
