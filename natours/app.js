af8c9e130b2d0336b176510d01b5eabfecef6b30
Bohdan Sych <bodya.flesh@gmail.com>
2020-08-18 22:07:47 +0300

    add rate limit

--- natours/app.js
+++ natours/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 
 const AppError = require('./utils/appError');
@@ -13,13 +14,17 @@ const app = express();
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
+
+//limiter
+const limiter = rateLimit({ 
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many requests from this IP, please try again in an hour'
+});
+app.use('/api', limiter);
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
-
-// app.use((req, res, next) => {
-//     console.log('Hello from the middleware');
-//     next();
-// });
 
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
