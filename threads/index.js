f68bf0e26bf4d2494e378fe282766f0d81465125
Bohdan Sych <bodya.flesh@gmail.com>
2020-08-24 11:21:42 +0300

    update

--- threads/index.js
+++ threads/index.js
@@ -1,16 +1,30 @@
-const express = require('express');
-const app = express();
+const cluster = require('cluster');
 
-function doWork(duration){
-    const start = Date.now();
-    while(Date.now() - start < duration){
 
+if(cluster.isMaster){
+    cluster.fork();
+    cluster.fork();
+    cluster.fork();
+    cluster.fork();
+}else{
+    const express = require('express');
+    const app = express();
+
+    function doWork(duration){
+        const start = Date.now();
+        while(Date.now() - start < duration){
+
+        }
     }
+
+    app.get('/', (req, res) => {
+        doWork(5000);
+        res.send('Hi there!');
+    });
+
+    app.get('/fast', (req, res) => {
+        res.send('This was fast!');
+    });
+
+    app.listen(3000);
 }
-
-app.get('/', (req, res) => {
-    doWork(5000);
-    res.send('Hi there!');
-});
-
-app.listen(3000);
