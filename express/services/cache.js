e7f26c3451c6134d5f7d80ce185eb286bd542be5
Bohdan Sych <bodya.flesh@gmail.com>
2020-08-24 22:54:08 +0300

    update

---
+++ express/services/cache.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const redis = require('redis');
+const { promisify } = require('util');
+
+const redisUrl = 'redis://127.0.0.1:6379';
+const client = redis.createClient(redisUrl);
+client.get = promisify(client.get);
+const exec = mongoose.Query.prototype.exec;
+
+mongoose.Query.prototype.exec = async function(){
+
+    const key = JSON.stringify(Object.assign({}, this.getQuery(), {
+        collection: this.mongooseCollection.name
+    }));
+
+
+    const cacheValue = await client.get(key);
+    if(cacheValue){
+
+    }
+
+    const result = await exec.apply(this, arguments);
+}
