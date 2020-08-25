024c9e2160d445f3d34475070b899741363fd772
Bohdan Sych <bodya.flesh@gmail.com>
2020-08-25 20:00:44 +0300

    update

--- express/routes/blogRoutes.js
+++ express/routes/blogRoutes.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
+const cleanCache = require('../middlewares/cleanCache');
 
 const Blog = mongoose.model('Blog');
 
@@ -14,7 +15,7 @@ module.exports = app => {
     });
 
     app.get('/api/blogs', requireLogin, async (req, res) => {
-        const blogs = await Blog.find({ _user: req.user.id });
+        const blogs = await Blog.find({ _user: req.user.id }).cache({ key: req.user.id });
         res.send(blogs);
     });
 
@@ -35,3 +36,9 @@ module.exports = app => {
         }
     });
 };
+
+module.exports = {
+    clearHash(hashKey){
+        client.del(JSON.stringify(hashKey));
+    }
+}
