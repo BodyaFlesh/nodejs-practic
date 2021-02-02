43edb6bf9019cc8f8953618ea98178621e1adec9
Bohdan Sych <bodya.flesh@gmail.com>
2021-02-02 09:44:08 +0200

    dependense

--- new-project/jwt-authentication/src/index.ts
+++ new-project/jwt-authentication/src/index.ts
@@ -1,9 +1,20 @@
 import "reflect-metadata";
 import {createConnection} from "typeorm";
 import {User} from "./entity/User";
+import * as express from 'express';
+import { Request, Response } from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req: Request, resp: Response) => {
+    resp.send("Hello there!");
+})
+app.listen(4000, () => console.log("Listening on port", 4000))
 
 createConnection().then(async connection => {
 
    
 
-}).catch(error => console.log(error));
+}).catch(error => console.log(error)); 
