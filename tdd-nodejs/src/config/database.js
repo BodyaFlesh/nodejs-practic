7d80443bc37480cda28d19e813299c1e5637af09
Bohdan Sych <bodya.flesh@gmail.com>
2021-02-03 13:13:24 +0200

    config

--- tdd-nodejs/src/config/database.js
+++ tdd-nodejs/src/config/database.js
@@ -1,8 +1,11 @@
 const Sequelize = require('sequelize');
+const config = require('config');
 
-const sequelize = new Sequelize("hoaxify", "serv_root", "jsdgw351;o94", {
-    dialect: "mysql",
-    logging: false
+const dbConfig = config.get("database");
+
+const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
+    dialect: dbConfig.dialect, 
+    logging: dbConfig.logging
 });
 
 module.exports = sequelize;
