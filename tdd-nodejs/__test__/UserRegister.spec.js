82426622f7c088a43de295c3d82e398eb718898d
Bohdan Sych <bodya.flesh@gmail.com>
2021-06-05 10:08:55 +0300

    update

--- tdd-nodejs/__test__/UserRegister.spec.js
+++ tdd-nodejs/__test__/UserRegister.spec.js
@@ -1,5 +1,7 @@
 const request = require('supertest');
-const app = require('../app');
+const app = require('../src/app');
+const User = require('../src/user/User');
+const sequelize = require('../src/config/database');
 
 describe('User Registration', () => {
   it('returns 200 OK when signup request is valid', (done) => {
@@ -26,6 +28,23 @@ describe('User Registration', () => {
       })
       .then((response) => {
         expect(response.body.message).toBe('User created');
+        done();
+      });
+  });
+
+  it('saves the user to database', (done) => {
+    request(app)
+      .post('/api/1.0/users')
+      .send({
+        username: 'user1',
+        email: 'user1@mail.com',
+        password: 'P4ssword',
+      })
+      .then(() => {
+        // query user table
+        User.findAll().then((userList) => {
+          expect(userList.length).toBe(1);
+        });
         done();
       });
   });
