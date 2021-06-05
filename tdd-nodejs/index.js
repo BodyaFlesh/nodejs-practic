d99e8ca0dfcf03230e35aa75b43e9a8958d6fa43
Bohdan Sych <bodya.flesh@gmail.com>
2021-06-05 09:32:26 +0300

    update

--- tdd-nodejs/index.js
+++ tdd-nodejs/index.js
@@ -1,6 +1,3 @@
-const app = require('./src/app');
-const sequelize = require('./src/config/database');
-
-sequelize.sync({force: true});
+const app = require('./app');
 
 app.listen(3000, () => console.log("app is running"));
