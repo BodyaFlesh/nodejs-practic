135a2f8d7225f2297372e73c6c6da80428c5c545
Bohdan Sych <bodya.flesh@gmail.com>
2021-06-16 07:07:27 +0300

    update

--- tdd-nodejs/src/user/UserRouter.js
+++ tdd-nodejs/src/user/UserRouter.js
@@ -2,19 +2,35 @@ const express = require('express');
 const UserService = require('./UserService');
 const router = express.Router();
 
-router.post('/api/1.0/users', async (req, res) => {
+const validateUsername = (req, res, next) => {
   const user = req.body;
   if (user.username === null) {
     return res.status(400).send({
       validationErrors: {
         username: 'Username cannot be null',
-      }
+      },
     });
   }
+  next();
+};
 
-  await UserService.save(req.body);
+const validateEmail = (req, res, next) => {
+  const user = req.body;
+  if (user.email === null) {
+    return res.status(400).send({
+      validationErrors: {
+        email: 'E-mail cannot be null', 
+      },
+    });
+  }
+  next();
+};
 
-  return res.send({ message: 'User created' });
+router.post('/api/1.0/users', validateUsername, validateEmail,
+  async (req, res) => {
+    await UserService.save(req.body);
+
+    return res.send({ message: 'User created' });
 });
 
 module.exports = router;
