d101031719449523bca698d24c07fcf105ae40a6
Bohdan Sych <bodya.flesh@gmail.com>
2021-06-18 12:37:29 +0300

    update activate user

--- tdd-nodejs/src/user/UserService.js
+++ tdd-nodejs/src/user/UserService.js
@@ -4,6 +4,7 @@ const crypto = require('crypto');
 const EmailService = require('../email/EmailService');
 const sequelize = require('../config/database');
 const EmailException = require('../email/EmailException');
+const InvalidTokenException = require('./InvalidTokenException');
 
 const generateToken = (length) => {
   return crypto.randomBytes(length).toString('hex').substring(0, length);
