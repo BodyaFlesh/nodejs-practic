4dd6f44ec41356d716b319c1d4c09a1a1c5ecbd6
Bohdan Sych <bodya.flesh@gmail.com>
2021-06-21 14:00:39 +0300

    update test

--- tdd-nodejs/__test__/UserListing.spec.js
+++ tdd-nodejs/__test__/UserListing.spec.js
@@ -11,15 +11,15 @@ beforeEach(() => {
   return User.destroy({ truncate: true });
 });
 
-const getUsers = async () => {
-  return await request(app).get('/api/1.0/users');
+const getUsers = () => {
+  return request(app).get('/api/1.0/users');
 };
 
 const addUsers = async (activeUserCount, inactiveUserCount = 0) => {
-  for(let i = 0; i < activeUserCount + inactiveUserCount; i++){
+  for (let i = 0; i < activeUserCount + inactiveUserCount; i++) {
     await User.create({
-      username: `user${i}`,
-      email: `user${i}@mail.com`,
+      username: `user${i + 1}`,
+      email: `user${i + 1}@mail.com`,
       inactive: i >= activeUserCount,
     });
   }
@@ -30,8 +30,7 @@ describe('Listing Users', () => {
     const response = await getUsers();
     expect(response.status).toBe(200);
   });
-
-  it('returns page object as response body', async () => {
+  it('reutns page object as response body', async () => {
     const response = await getUsers();
     expect(response.body).toEqual({
       content: [],
@@ -40,59 +39,60 @@ describe('Listing Users', () => {
       totalPages: 0,
     });
   });
-
   it('returns 10 users in page content when there are 11 users in database', async () => {
     await addUsers(11);
-
     const response = await getUsers();
     expect(response.body.content.length).toBe(10);
   });
-
-  it('returns 6 users in page content when there are active 6 users and inactive users in database', async () => {
+  it('returns 6 users in page content when there are active 6 users and inactive 5 users in database', async () => {
     await addUsers(6, 5);
     const response = await getUsers();
     expect(response.body.content.length).toBe(6);
   });
-
   it('returns only id, username and email in content array for each user', async () => {
-    await addUsers(6, 5);
+    await addUsers(11);
     const response = await getUsers();
     const user = response.body.content[0];
     expect(Object.keys(user)).toEqual(['id', 'username', 'email']);
   });
-
-  it('returs 2 as totalPages when thre are 15 active and 7 inactive users', async () => {
+  it('returns 2 as totalPages when there are 15 active and 7 inactive users', async () => {
     await addUsers(15, 7);
-
     const response = await getUsers();
     expect(response.body.totalPages).toBe(2);
   });
-
-  it('returns second page users and page incicator when page in set as 1 in request parameter', async () => {
+  it('returns second page users and page indicator when page is set as 1 in request parameter', async () => {
     await addUsers(11);
     const response = await getUsers().query({ page: 1 });
-    expect(response.body.content[0].username).toBe('user10');
+    expect(response.body.content[0].username).toBe('user11');
     expect(response.body.page).toBe(1);
   });
-
   it('returns first page when page is set below zero as request parameter', async () => {
     await addUsers(11);
-    const response = await getUsers().query({ page: -1 });
-    expect(response.body.content[0].username).toBe('user10');
+    const response = await getUsers().query({ page: -5 });
     expect(response.body.page).toBe(0);
   });
-
-  it('returns 5 users and corresponding size indicztor when size is set as 5 in request parametr', async () => {
+  it('returns 5 users and corresponding size indicator when size is set as 5 in request parameter', async () => {
     await addUsers(11);
     const response = await getUsers().query({ size: 5 });
     expect(response.body.content.length).toBe(5);
     expect(response.body.size).toBe(5);
   });
-
   it('returns 10 users and corresponding size indicator when size is set as 1000', async () => {
     await addUsers(11);
     const response = await getUsers().query({ size: 1000 });
     expect(response.body.content.length).toBe(10);
     expect(response.body.size).toBe(10);
+  });
+  it('returns 10 users and corresponding size indicator when size is set as 0', async () => {
+    await addUsers(11);
+    const response = await getUsers().query({ size: 0 });
+    expect(response.body.content.length).toBe(10);
+    expect(response.body.size).toBe(10);
+  });
+  it('returns page as zero and size as 10 when non numeric query params provided for both', async () => {
+    await addUsers(11);
+    const response = await getUsers().query({ size: 'size', page: 'page' });
+    expect(response.body.size).toBe(10);
+    expect(response.body.page).toBe(0);
   });
 });
